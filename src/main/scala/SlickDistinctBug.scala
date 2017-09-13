c6a3442ba14e8058ef6f7c5249ea1097a36d6e16
FGabor <fugafree@gmail.com>
2017-09-13 09:50:31 +0200

    User with more columns

--- src/main/scala/SlickDistinctBug.scala
+++ src/main/scala/SlickDistinctBug.scala
@@ -1,7 +1,8 @@
-import scala.concurrent.{Future, Await}
+import slick.driver.H2Driver.api._
+
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
-import slick.driver.H2Driver.api._
+import scala.concurrent.{Await, Future}
 
 object SlickDistinctBug extends App {
   val db = Database.forConfig("h2mem1")
@@ -11,11 +12,10 @@ object SlickDistinctBug extends App {
 
     val setupAction: DBIO[Unit] = DBIO.seq(
       users.schema.create,
-      users += User("John Doe", "Groundsville"),
-      users += User("Fred Smith", "Mendocino"),
-      users += User("Big Guy", "Meadows"),
-      users += User("Little Washington", "New York"),
-      users += User("Stu", "Meadows")
+      users += User("John Doe", 167, true, "Meadows"),
+      users += User("John Doe", 167, true, "Meadows"),
+      users += User("Fred Smith", 189, false, "Mendocino"),
+      users += User("Big Guy", 195, true, "Meadows")
     )
 
     val setupFuture: Future[Unit] = db.run(setupAction)
@@ -46,13 +46,17 @@ object SlickDistinctBug extends App {
 
 case class User(
     name: String,
+    height: Int,
+    hasDog: Boolean,
     city: String,
     id: Option[Int] = None)
 
 class Users(tag: Tag) extends Table[User](tag, "USERS") {
   def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
   def name = column[String]("NAME")
+  def height = column[Int]("HEIGHT")
+  def hasDog = column[Boolean]("HAS_DOG")
   def city = column[String]("CITY")
 
-  def * = (name, city, id.?) <> (User.tupled, User.unapply)
+  def * = (name, height, hasDog, city, id.?) <> (User.tupled, User.unapply)
 }
