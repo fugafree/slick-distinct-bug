ddcd8ce1439974d7961b91e5eb7ceea425bab53c
FGabor <fugafree@gmail.com>
2017-09-13 09:44:21 +0200

    a working mapped distinct example with case class mapped User

--- src/main/scala/SlickDistinctBug.scala
+++ src/main/scala/SlickDistinctBug.scala
@@ -3,53 +3,38 @@ import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration.Duration
 import slick.driver.H2Driver.api._
 
-// The main application
 object SlickDistinctBug extends App {
   val db = Database.forConfig("h2mem1")
   try {
 
-    // The query interface for the Suppliers table
-    val suppliers: TableQuery[Suppliers] = TableQuery[Suppliers]
+    val users: TableQuery[Users] = TableQuery[Users]
 
     val setupAction: DBIO[Unit] = DBIO.seq(
-      // Create the schema by combining the DDLs for the Suppliers
-      // tables using the query interfaces
-      suppliers.schema.create,
-
-      // Insert some suppliers
-      suppliers += (101, "Acme, Inc.", "Groundsville"),
-      suppliers += ( 49, "Superior Coffee", "Mendocino"),
-      suppliers += (150, "The High Ground", "Meadows"),
-      suppliers += ( 21, "The Hole", "New York"),
-      suppliers += ( 81, "Great Sup", "Meadows")
+      users.schema.create,
+      users += User("John Doe", "Groundsville"),
+      users += User("Fred Smith", "Mendocino"),
+      users += User("Big Guy", "Meadows"),
+      users += User("Little Washington", "New York"),
+      users += User("Stu", "Meadows")
     )
 
     val setupFuture: Future[Unit] = db.run(setupAction)
     val f = setupFuture.flatMap { _ =>
 
-      val allSuppliersAction: DBIO[Seq[(Int, String, String)]] =
-        suppliers.result
+      val allUsersFuture: Future[Seq[User]] = db.run(users.result)
 
-      val allSuppliersFuture: Future[Seq[(Int, String, String)]] =
-        db.run(allSuppliersAction)
-
-      allSuppliersFuture.map { allSuppliers =>
+      allUsersFuture.map { allSuppliers =>
         allSuppliers.foreach(println)
       }
 
     }.flatMap { _ =>
 
-      /* Filtering / Where */
-
-      // Construct a query where the price of Coffees is > 9.0
       val filterQuery: Query[Rep[String], String, Seq] =
-        suppliers.filter(_.city like "M%").map(_.city).distinct
+        users.filter(_.city like "M%").map(_.city).distinct
 
-      // Print the SQL for the filter query
       println("Generated SQL for filter query:\n" + filterQuery.result.statements)
 
       println("Distinct cities starts with 'M':")
-      // Execute the query and print the Seq of results
       db.run(filterQuery.result.map(println))
 
     }
@@ -57,4 +42,17 @@ object SlickDistinctBug extends App {
     Await.result(f, Duration.Inf)
 
   } finally db.close
+}
+
+case class User(
+    name: String,
+    city: String,
+    id: Option[Int] = None)
+
+class Users(tag: Tag) extends Table[User](tag, "USERS") {
+  def id = column[Int]("ID", O.PrimaryKey, O.AutoInc)
+  def name = column[String]("NAME")
+  def city = column[String]("CITY")
+
+  def * = (name, city, id.?) <> (User.tupled, User.unapply)
 }
