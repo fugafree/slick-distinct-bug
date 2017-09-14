a83edc4acd85cab2cae7cef7c943f83b5e0557fc
FGabor <fugafree@gmail.com>
2017-09-14 09:24:21 +0200

    A cleanup which caused that the distinct statement now fails. I don't now why. Sometimes it fails sometimes it doesn't.

--- src/main/scala/SlickDistinctBug.scala
+++ src/main/scala/SlickDistinctBug.scala
@@ -5,8 +5,15 @@ import scala.concurrent.duration.Duration
 import scala.concurrent.{Await, Future}
 
 object SlickDistinctBug extends App {
+
+  def runAndWait[T](dbio: DBIO[T]): T = {
+    Await.result(db.run(dbio), Duration.Inf)
+  }
+
   val db = Database.forConfig("h2mem1")
   try {
+
+    println("\n\n ####### Start of test #######")
 
     val users: TableQuery[Users] = TableQuery[Users]
 
@@ -15,35 +22,38 @@ object SlickDistinctBug extends App {
       users += User("John Doe", 167, true, "Meadows"),
       users += User("John Doe", 167, true, "Meadows"),
       users += User("Fred Smith", 189, false, "Mendocino"),
-      users += User("Big Guy", 195, true, "Meadows")
+      users += User("Big Guy", 195, true, "New York")
     )
 
-    val setupFuture: Future[Unit] = db.run(setupAction)
-    val f = setupFuture.flatMap { _ =>
+    runAndWait(setupAction)
 
-      val allUsersFuture: Future[Seq[User]] = db.run(users.result)
+    val allUsers: Seq[User] = runAndWait(users.result)
+    allUsers.foreach(println)
 
-      allUsersFuture.map { allSuppliers =>
-        allSuppliers.foreach(println)
-      }
+    val distinctQuery: Query[Rep[User], User, Seq] = users.filter(_.city like "M%").distinct
 
-    }.flatMap { _ =>
+    println("Generated SQL queries for distinct statement. These are from the same query with multiple .result call:")
+    println(distinctQuery.result.statements)
+    println(distinctQuery.result.statements)
+    println(distinctQuery.result.statements)
+    println(distinctQuery.result.statements)
+    println(distinctQuery.result.statements)
+    println(distinctQuery.result.statements)
 
-      // val filterQuery: Query[Rep[String], String, Seq] =
-      //   users.filter(_.city like "M%").map(_.city).distinct
-      val filterQuery: Query[Rep[User], User, Seq] =
-        users.distinct
-
-      println("Generated SQL for filter query:\n" + filterQuery.result.statements)
-
-      println("Distinct users")
-      db.run(filterQuery.result.map(println))
-
+    println("Distinct users:")
+    try {
+      val distinctResult = runAndWait(distinctQuery.result)
+      println(s"Result of distinct query: $distinctResult")
+    } catch {
+      case e: Throwable =>
+        println(s"!!!!!! Distinct query run failed with exception: ${e.getMessage}")
+        println(s"       Cause: ${e.getCause.getMessage}")
     }
 
-    Await.result(f, Duration.Inf)
-
-  } finally db.close
+  } finally {
+    println("####### End of test, closing db connection #######\n\n")
+    db.close
+  }
 }
 
 case class User(
